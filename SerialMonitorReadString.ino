5a48a8ee1ce21a9d88843d73dcc60dfe51c21c6a
Aditya Rao <speedbirdlives@gmail.com>
2026-09-13 16:36:38 +0530

    Update SerialMonitorReadString.ino
    Added necessary comments.

--- SerialMonitorReadString.ino
+++ SerialMonitorReadString.ino
@@ -1,3 +1,6 @@
+// Program to demonstrate String user input using Serial Monitor.
+
+//Initialising variables
 String myName;
 String msg = "What is your Name? ";
 String msg2 = "Hello ";
@@ -7,16 +10,22 @@ int wait = 1000;
 
 void setup() {
   // put your setup code here, to run once:
+
+  //Setting up serial monitor
 Serial.begin(57600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  //Using standard user input procedure.
 Serial.println(msg);
 while(Serial.available()==0){
 
 }
-myName = Serial.readString();
+myName = Serial.readString(); //Parsing string from user input.
+
+  //Displaying messages as follows.
 Serial.print(msg);
 Serial.print(myName);
 Serial.println(msg2);
