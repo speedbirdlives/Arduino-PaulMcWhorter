5aa9147559936c0c43d66c30a2062894066e7779
Aditya Rao <speedbirdlives@gmail.com>
2026-09-13 12:15:05 +0530

    Update LEDBlink.ino
    Added required Comments

--- LEDBlink.ino
+++ LEDBlink.ino
@@ -1,5 +1,7 @@
 void setup() {
   // put your setup code here, to run once:
+
+  //initailising all three pins to mode=output.
 pinMode(8,OUTPUT);
 pinMode(13,OUTPUT);
 pinMode(12,OUTPUT);
@@ -7,16 +9,20 @@ pinMode(12,OUTPUT);
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  //blinking the first LED
 digitalWrite(8,HIGH);
 delay(100);
 digitalWrite(8,LOW);
 delay(100);
 
+  //blinking the second LED
 digitalWrite(12,HIGH);
 delay(100);
 digitalWrite(12,LOW);
 delay(100);
 
+  //blinking the third LED
 digitalWrite(13,HIGH);
 delay(100);
 digitalWrite(13,LOW);
