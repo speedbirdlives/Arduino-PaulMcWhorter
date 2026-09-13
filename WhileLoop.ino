93c8facb4ae16998ffeec453ad3125345feb8da7
Aditya Rao <speedbirdlives@gmail.com>
2026-09-13 20:27:04 +0530

    Update WhileLoop.ino
    Added necessary comments.

--- WhileLoop.ino
+++ WhileLoop.ino
@@ -1,17 +1,23 @@
+//Simple program to demonstrate while loop using Arduino.
 
+//Iteration variable etc.
 int i;
 int wait = 500;
 
 
 
+
 void setup() {
   // put your setup code here, to run once:
+
+  //Setting up serial monitor.
 Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  //Basic while loop to print 1-10
   i=1;
 while(i<=10){
   Serial.println(i);
@@ -19,6 +25,5 @@ while(i<=10){
   delay(wait);
 }
 
-Serial.println(" ");
-
+Serial.println(" ");//Space between two main iterations(void loop)
 }
