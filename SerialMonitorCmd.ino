e5a269c60be94653cf3d6fc5240d6d1d665e8855
Aditya Rao <speedbirdlives@gmail.com>
2026-09-13 16:27:47 +0530

    Update SerialMonitorCmd.ino
    Added necessary comments.

--- SerialMonitorCmd.ino
+++ SerialMonitorCmd.ino
@@ -1,4 +1,6 @@
+//program to display working of serial monitor using infinite loop.
 
+//initialising the variables.
 int j=1;
 int wait = 1000;
 String str = "j = ";
@@ -6,11 +8,16 @@ String str = "j = ";
 
 void setup() {
   // put your setup code here, to run once:
+
+  //Setting up the Serial Monitor
 Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  //Infinite main loop.
+  //It just goes on adding 1 to j and displaying.
 Serial.print(str);
 Serial.println(j);
 j=j+1;
