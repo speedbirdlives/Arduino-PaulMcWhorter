26c08131739f34e21702d30fc458e6cb3ceec771
Aditya Rao <speedbirdlives@gmail.com>
2026-09-13 12:30:35 +0530

    Add LEDSOSMorse.ino via upload.

---
+++ LEDSOSMorse.ino
@@ -0,0 +1,49 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(8,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(8,HIGH);
+delay(300);
+digitalWrite(8,LOW);
+delay(300);
+digitalWrite(8,HIGH);
+delay(300);
+digitalWrite(8,LOW);
+delay(300);
+digitalWrite(8,HIGH);
+delay(300);
+digitalWrite(8,LOW);
+delay(300);
+
+digitalWrite(8,HIGH);
+delay(700);
+digitalWrite(8,LOW);
+delay(700);
+digitalWrite(8,HIGH);
+delay(700);
+digitalWrite(8,LOW);
+delay(700);
+digitalWrite(8,HIGH);
+delay(700);
+digitalWrite(8,LOW);
+delay(700);
+
+digitalWrite(8,HIGH);
+delay(300);
+digitalWrite(8,LOW);
+delay(300);
+digitalWrite(8,HIGH);
+delay(300);
+digitalWrite(8,LOW);
+delay(300);
+digitalWrite(8,HIGH);
+delay(300);
+digitalWrite(8,LOW);
+delay(300);
+
+delay(2000);
+}
