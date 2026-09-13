b36667d099f6661926028af647bc841fbbe180a3
Aditya Rao <speedbirdlives@gmail.com>
2026-09-13 18:11:42 +0530

    Update BuzzerWarning.ino
    Added necessary comments and added a missing else statement. Also suggested the user to enter only a natural number for easier and simpler processing.

--- BuzzerWarning.ino
+++ BuzzerWarning.ino
@@ -1,29 +1,45 @@
-int myNum;
-int Buzpin = 6;
+//Simple program to demonstrate an Active buzzer.
+//If the natural number entered by the user is above 10, the buzzer will beep.
+
+int myNum; //user entered number
+int Buzpin = 6; //Buzzer pin
 int wait = 1000;
-String ask = "Enter a number: ";
+
+//Strings as necessary
+String ask = "Enter a natural number: "; 
 String warning = "Your number is above 10.";
 
 
 void setup() {
   // put your setup code here, to run once:
+
+  //Setting up serial monitor and buzzer pin
 Serial.begin(9600);
 pinMode(Buzpin,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  //User input for an integer
 Serial.println(ask);
 while(Serial.available()==0){
 
 }
-myNum = Serial.parseInt();
+myNum = Serial.parseInt(); //Parsing user input 
 
+  //logic
 if (myNum>10)
 {
-  Serial.println(warning);
+  Serial.println(warning); //Warning message
+  
+  //One buzzer beep
   digitalWrite(Buzpin,HIGH);
   delay(wait);
+  digitalWrite(Buzpin,LOW);
+}
+
+else{
   digitalWrite(Buzpin,LOW);
 }
 
