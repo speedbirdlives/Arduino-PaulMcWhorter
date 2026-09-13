c19f704ed6792befb3a6b252cea46b521c8f789c
Aditya Rao <speedbirdlives@gmail.com>
2026-09-13 16:55:59 +0530

    Update SOSwithVariables.ino
    Added necessary comments.

--- SOSwithVariables.ino
+++ SOSwithVariables.ino
@@ -1,19 +1,29 @@
+// Same SOS morse code program using delay variables
 
-int redLED=8;
-float pi=3.142;
-int dit=300;
-int dah=700;
+//Setting up variables
+
+int redLED=8; // Red led at pin 8
+float pi=3.142; 
+
+int dit=300; // dit represents "."
+int dah=700; // dah represents "-"
 
 
 
 void setup() {
   // put your setup code here, to run once:
+
+  //setting up pin to output
 pinMode(redLED,OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  // SOS in morse is ... --- ... (S,O,S)
+
+  // S => ... (dit dit dit)
 digitalWrite(redLED,HIGH);
 delay(300);
 digitalWrite(redLED,LOW);
@@ -27,6 +37,7 @@ delay(300);
 digitalWrite(redLED,LOW);
 delay(300);
 
+  // O => --- (dah dah dah)
 digitalWrite(redLED,HIGH);
 delay(700);
 digitalWrite(redLED,LOW);
@@ -40,6 +51,8 @@ delay(700);
 digitalWrite(redLED,LOW);
 delay(700);
 
+  
+// S => ... (dit dit dit)
 digitalWrite(redLED,HIGH);
 delay(300);
 digitalWrite(redLED,LOW);
@@ -53,6 +66,7 @@ delay(300);
 digitalWrite(redLED,LOW);
 delay(300);
 
+  // final delay between two SOS signals to seperate them.
 delay(2000);
 }
 
