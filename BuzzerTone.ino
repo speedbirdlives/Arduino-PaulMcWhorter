b21348e977b4595df78aa5d35e29015f5938d7f4
Aditya Rao <speedbirdlives@gmail.com>
2026-09-13 18:17:24 +0530

    Update BuzzerTone.ino
    Added necessary comments.

--- BuzzerTone.ino
+++ BuzzerTone.ino
@@ -1,7 +1,12 @@
+//Simple program to generate a kind of beeping tone in a Buzzer.
 
-int buzzpin = 8;
-int wait1 = 100;
-int wait2 = 200;
+//Initialising variables
+
+int buzzpin = 8; //Buzzer pin
+int wait1 = 100; //1st beep delay
+int wait2 = 200; //2nd beep delay
+
+//Counter variables
 int i;
 int j;
 
@@ -9,21 +14,24 @@ int j;
 
 void setup() {
   // put your setup code here, to run once:
+
+  //Buzzer Pin setup
 pinMode(buzzpin,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  // For 100 iterations, we have two sets of beeps
 for(i=1;i<=100;i++){
 
-
+//Beep 1
   digitalWrite(buzzpin,HIGH);
   delay(wait1);
   digitalWrite(buzzpin,LOW);
   delay(wait1);
 
-
+//Beep 2
   digitalWrite(buzzpin,HIGH);
   delay(wait2);
   digitalWrite(buzzpin,LOW);
