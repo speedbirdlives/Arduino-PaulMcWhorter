4d167a1e4248e496af78f64199f9e7c62b183232
Aditya Rao <speedbirdlives@gmail.com>
2026-09-13 20:10:02 +0530

    Update RGBColorMixing.ino
    Added necessary comments.

--- RGBColorMixing.ino
+++ RGBColorMixing.ino
@@ -1,3 +1,7 @@
+//Simple program to mix colors of an RGB led to get new colors.
+
+
+//Initialising pin variables
 int red = 8;
 int green = 9;
 int blue = 10;
@@ -6,6 +10,8 @@ int blue = 10;
 
 void setup() {
   // put your setup code here, to run once:
+
+  //Setting up pin modes.
 pinMode(red,OUTPUT);
 pinMode(green,OUTPUT);
 pinMode(blue,OUTPUT);
@@ -13,6 +19,8 @@ pinMode(blue,OUTPUT);
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  //Edit the digitalWrite commands using the legend below to experiment with the colors.
 digitalWrite(red,LOW);
 digitalWrite(green,HIGH);
 digitalWrite(blue,HIGH);
