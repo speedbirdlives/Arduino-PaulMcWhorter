82396d1bd8de8c1d0d6aee65f4e4926461cc37bd
Aditya Rao <speedbirdlives@gmail.com>
2026-09-13 16:32:31 +0530

    Update SerialMonitorRead.ino
    Added necessary comments.

--- SerialMonitorRead.ino
+++ SerialMonitorRead.ino
@@ -1,20 +1,32 @@
+// Program to use Serial monitor to take integer input from the user.
+
+
+//initialising variables
 int num;
 int wait = 2000;
 String msg = "Enter the number: ";
 String msg2 = "Your number is: ";
 void setup() {
   // put your setup code here, to run once:
+
+  //Setting up serial monitor
 Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-Serial.println(msg);
+
+  //waiting for the user to enter the number using while loop.
+  //Basically, while the serial monitor is empty, there is an empty infinite loop until the serial monitor gets some data.
+  
+Serial.println(msg); //Enter the number
 while(Serial.available()==0){
 
 }
-num = Serial.parseInt();
-Serial.print(msg2);
-Serial.println(num);
+  
+num = Serial.parseInt(); //Parsing integer from user input.
+  
+Serial.print(msg2); //Your number is..
+Serial.println(num); //Printing the user entered number.
 delay(wait);
 }
