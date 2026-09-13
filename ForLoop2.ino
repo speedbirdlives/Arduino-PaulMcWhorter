c3dea4dcb0d668132153dd5b14d2189ccebddfb9
Aditya Rao <speedbirdlives@gmail.com>
2026-09-13 12:46:13 +0530

    Update ForLoop2.ino
    Added necessary comments.

--- ForLoop2.ino
+++ ForLoop2.ino
@@ -1,4 +1,7 @@
+//program for a counter using for loop.
+//Board = Arduino UNO R3
 
+//Initailising looping variables
 int i;
 int j;
 int wait = 500;
@@ -6,14 +9,18 @@ int wait = 500;
 
 void setup() {
   // put your setup code here, to run once:
+
+  //Serial monitor setup
 Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  //Using for loop to print 1-10
 for(j=1;j<=10;j++){
   Serial.println(j);
   delay(wait);
 }
-Serial.println(" ");
+Serial.println(" "); // Seperating the main iterations(void loop)
 }
