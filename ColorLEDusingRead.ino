63e49c14339733138f1d28f9a500eefff45eb013
Aditya Rao <speedbirdlives@gmail.com>
2026-09-13 20:01:53 +0530

    Add ColorLEDusingRead.ino via upload.

---
+++ ColorLEDusingRead.ino
@@ -0,0 +1,47 @@
+int red =6;
+int green =7;
+int blue =8;
+
+int wait = 2000;
+
+String ask = "Enter the color in all small letters: ";
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(red,OUTPUT);
+pinMode(green,OUTPUT);
+pinMode(blue,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.println(ask);
+
+while(Serial.available() == 0){
+
+}
+ask = Serial.readString();
+
+if(ask == "red"){
+  digitalWrite(red,HIGH);
+  digitalWrite(green,LOW);
+  digitalWrite(blue,LOW);
+}
+if(ask == "green"){
+  digitalWrite(red,LOW);
+  digitalWrite(green,HIGH);
+  digitalWrite(blue,LOW);
+}
+if(ask == "blue"){
+  digitalWrite(red,LOW);
+  digitalWrite(green,LOW);
+  digitalWrite(blue,HIGH);
+}
+
+
+delay(wait);
+
+}
