3511a4ea563ccc131d746656efbcd6564b6933ba
Aditya Rao <speedbirdlives@gmail.com>
2026-09-13 20:16:29 +0530

    Update Photoresistor-Photodetector.ino
    Added necessary comments.

--- Photoresistor-Photodetector.ino
+++ Photoresistor-Photodetector.ino
@@ -1,13 +1,20 @@
+//Simple program which uses photoresistor to detect light intensity and turn on respective led.
+//High intensity => Low resistance
+//Low intensity => High resistance
 
-int lightPin = A0;
-int LightVal;
+//Initialising variables
+int lightPin = A0; //Photoresistor analogRead pin
+int LightVal; //Analogread resistance value
 int wait = 1000;
 
+//LEDPin variables
 int red=8;
 int green=9;
 
 void setup() {
   // put your setup code here, to run once:
+
+  //Setting up serial monitor and light pins.
 pinMode(lightPin,INPUT);
 pinMode(red,OUTPUT);
 pinMode(green,OUTPUT);
@@ -16,17 +23,23 @@ Serial.begin(9600);
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  //Analog reading photoresistor resistance
 LightVal = analogRead(lightPin);
 Serial.println(LightVal);
 
+//LOW intensity => Green on and Red off.
 if(LightVal>=400){
   digitalWrite(green,HIGH);
   digitalWrite(red,LOW);
 }
+
+//HIGH intensity => Green off and Red on.
 if(LightVal<400){
   digitalWrite(red,HIGH);
   digitalWrite(green,LOW);
 }
 
+  //Wait time for main loop.
 delay(wait);
 }
