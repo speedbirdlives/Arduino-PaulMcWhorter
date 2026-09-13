b572b0be330d8e439208fa55a231810f2acb8504
Aditya Rao <speedbirdlives@gmail.com>
2026-09-13 12:43:08 +0530

    Add ForLoop2.ino via upload

---
+++ ForLoop2.ino
@@ -0,0 +1,19 @@
+
+int i;
+int j;
+int wait = 500;
+
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+for(j=1;j<=10;j++){
+  Serial.println(j);
+  delay(wait);
+}
+Serial.println(" ");
+}
