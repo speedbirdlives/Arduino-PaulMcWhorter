59dd1d6ca55b2196bdcc89ef21246eb77ab85334
Aditya Rao <speedbirdlives@gmail.com>
2026-09-13 20:24:10 +0530

    Add WhileLoop.ino via upload.

---
+++ WhileLoop.ino
@@ -0,0 +1,24 @@
+
+int i;
+int wait = 500;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  i=1;
+while(i<=10){
+  Serial.println(i);
+  i++;
+  delay(wait);
+}
+
+Serial.println(" ");
+
+}
