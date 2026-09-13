d49e7e67166a6ea4e76f36cb8d5360f3d0c7044f
Aditya Rao <speedbirdlives@gmail.com>
2026-09-13 12:23:03 +0530

    Update and rename LEDBinaryCounter1-15.ino to LEDBinaryCounter0-15.ino
    Added required comments and changed the name to appropriate(correct) name.

--- LEDBinaryCounter0-15.ino
+++ LEDBinaryCounter0-15.ino
@@ -1,4 +1,4 @@
-//program to count 1-15 in binary using 4 LEDs in on/off configs.
+//program to count 0-15 in binary using 4 LEDs in on/off configs.
 
 
 int LED1 = 2;
@@ -13,6 +13,7 @@ int wait = 3000;
 void setup() {
   // put your setup code here, to run once:
 
+  // configuring all the four LED pins to mode=output.
 pinMode(LED1,OUTPUT);
 pinMode(LED2,OUTPUT);
 pinMode(LED3,OUTPUT);
@@ -24,6 +25,8 @@ pinMode(LED4,OUTPUT);
 void loop() {
   // put your main code here, to run repeatedly:
 
+  
+// Decimal=0 Binary=0000
 digitalWrite(LED1,LOW);
 digitalWrite(LED2,LOW);
 digitalWrite(LED3,LOW);
@@ -31,7 +34,7 @@ digitalWrite(LED4,LOW);
 delay(wait);
 
 
-
+// Decimal=1 Binary=0001
 digitalWrite(LED1,LOW);
 digitalWrite(LED2,LOW);
 digitalWrite(LED3,LOW);
@@ -40,7 +43,7 @@ delay(wait);
 
 
 
-
+// Decimal=2 Binary=0010
 digitalWrite(LED1,LOW);
 digitalWrite(LED2,LOW);
 digitalWrite(LED3,HIGH);
@@ -49,7 +52,7 @@ delay(wait);
 
 
 
-
+// Decimal=3 Binary=0011
 digitalWrite(LED1,LOW);
 digitalWrite(LED2,LOW);
 digitalWrite(LED3,HIGH);
@@ -58,7 +61,7 @@ delay(wait);
 
 
 
-
+//Decimal =4 Binary=0100
 digitalWrite(LED1,LOW);
 digitalWrite(LED2,HIGH);
 digitalWrite(LED3,LOW);
@@ -67,7 +70,7 @@ delay(wait);
 
 
 
-
+//Decimal=5 Binary=0101
 digitalWrite(LED1,LOW);
 digitalWrite(LED2,HIGH);
 digitalWrite(LED3,LOW);
@@ -75,6 +78,7 @@ digitalWrite(LED4,HIGH);
 delay(wait);
 
 
+  // Decimal=6 Binary=0110
 digitalWrite(LED1,LOW);
 digitalWrite(LED2,HIGH);
 digitalWrite(LED3,HIGH);
@@ -82,7 +86,7 @@ digitalWrite(LED4,LOW);
 delay(wait);
 
 
-
+// Decimal=7 BInary=0111
 digitalWrite(LED1,LOW);
 digitalWrite(LED2,HIGH);
 digitalWrite(LED3,HIGH);
@@ -91,7 +95,7 @@ delay(wait);
 
 
 
-
+//Decimal=8 BInary=1000
 digitalWrite(LED1,HIGH);
 digitalWrite(LED2,LOW);
 digitalWrite(LED3,LOW);
@@ -100,7 +104,7 @@ delay(wait);
 
 
 
-
+//Decimal=9 Binary=1001
 digitalWrite(LED1,HIGH);
 digitalWrite(LED2,LOW);
 digitalWrite(LED3,LOW);
@@ -108,7 +112,7 @@ digitalWrite(LED4,HIGH);
 delay(wait);
 
 
-
+//Decimal=10 Binary=1010
 digitalWrite(LED1,HIGH);
 digitalWrite(LED2,LOW);
 digitalWrite(LED3,HIGH);
@@ -117,7 +121,7 @@ delay(wait);
 
 
 
-
+//Decimal=11 Binary=1011
 digitalWrite(LED1,HIGH);
 digitalWrite(LED2,LOW);
 digitalWrite(LED3,HIGH);
@@ -125,7 +129,7 @@ digitalWrite(LED4,HIGH);
 delay(wait);
 
 
-
+//Decimal=12 Binary=1100
 digitalWrite(LED1,HIGH);
 digitalWrite(LED2,HIGH);
 digitalWrite(LED3,LOW);
@@ -134,7 +138,7 @@ delay(wait);
 
 
 
-
+// Decimal=13 BInary=1101
 digitalWrite(LED1,HIGH);
 digitalWrite(LED2,HIGH);
 digitalWrite(LED3,LOW);
@@ -142,7 +146,7 @@ digitalWrite(LED4,HIGH);
 delay(wait);
 
 
-
+//Decimal=14 Binary=1110
 digitalWrite(LED1,HIGH);
 digitalWrite(LED2,HIGH);
 digitalWrite(LED3,HIGH);
@@ -151,7 +155,7 @@ delay(wait);
 
 
 
-
+//Decimal=15 Binary=1111
 digitalWrite(LED1,HIGH);
 digitalWrite(LED2,HIGH);
 digitalWrite(LED3,HIGH);
