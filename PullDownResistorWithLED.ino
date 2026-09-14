cb98f5454cf13f7f2501b7a15cc96e811309a6b6
Aditya Rao <speedbirdlives@gmail.com>
2026-09-14 11:48:07 +0530

    Update PullDownResistorWithLED.ino
    Added necessary comments.

--- PullDownResistorWithLED.ino
+++ PullDownResistorWithLED.ino
@@ -1,3 +1,6 @@
+//Program to demonstrate the use of a Pull down resistor with a LED.
+
+//variables and parameters
 int ledpin = 8;
 int buttonpin = 12;
 int buttonread;
@@ -7,6 +10,8 @@ int wait = 500;
 
 void setup() {
   // put your setup code here, to run once:
+
+//Setting up serial monitor and pins.
 Serial.begin(9600);
 pinMode(ledpin,OUTPUT);
 pinMode(buttonpin,INPUT);
@@ -14,11 +19,15 @@ pinMode(buttonpin,INPUT);
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+//reading from button.
 buttonread = digitalRead(buttonpin);
 Serial.println(buttonread);
 
 delay(wait);
 
+//Logic
+//LED turns on when button is pushed. Off when button not pushed.
 if(buttonread==0){
   digitalWrite(ledpin,LOW);
   Serial.println("NOT PUSHED!");
