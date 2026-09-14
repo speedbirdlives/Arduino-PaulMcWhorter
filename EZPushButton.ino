e22eb71ae1fb1114de9dec1557344ba5943038e2
Aditya Rao <speedbirdlives@gmail.com>
2026-09-14 12:27:36 +0530

    Update EZPushButton.ino
    Added necessary comments.

--- EZPushButton.ino
+++ EZPushButton.ino
@@ -1,4 +1,6 @@
+//Program for an easy way to use a Push button by Paul McWhorter
 
+//Required variables and parameters
 int buttonPin = 2;
 int buttonValue;
 
@@ -6,6 +8,8 @@ int buttonValue;
 
 void setup() {
   // put your setup code here, to run once:
+
+//setting up pins and serial monitor
 pinMode(buttonPin,INPUT);
 digitalWrite(buttonPin,HIGH);
 Serial.begin(9600);
@@ -13,6 +17,10 @@ Serial.begin(9600);
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+//logic
 buttonValue = digitalRead(buttonPin);
 Serial.println(buttonValue);
+
+//We implement this buttonValue in any usage cases.
 }
