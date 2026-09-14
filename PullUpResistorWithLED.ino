34cc4d3e6a8ce240b503fb4cfce3c5512881332a
Aditya Rao <speedbirdlives@gmail.com>
2026-09-14 11:55:33 +0530

    Update PullUpResistorWithLED.ino
    Add necessary comments.

--- PullUpResistorWithLED.ino
+++ PullUpResistorWithLED.ino
@@ -1,3 +1,6 @@
+//Program to demonstrate Pull Up resistor with LED.
+
+//Required parameters
 int LEDPin = 8;
 int buttonPin = 12;
 int buttonread;
@@ -6,6 +9,8 @@ int wait = 500;
 
 void setup() {
   // put your setup code here, to run once:
+
+//Setting up pins and serial monitor.
 Serial.begin(9600);
 pinMode(LEDPin,OUTPUT);
 pinMode(buttonPin,INPUT);
@@ -13,10 +18,14 @@ pinMode(buttonPin,INPUT);
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+//reading from the button
 buttonread = digitalRead(buttonPin);
 Serial.println(buttonread);
 delay(wait);
 
+//Logic
+//LED turns on if button is pushed. Off if button is not pushed.
 if(buttonread == 1){
   digitalWrite(LEDPin,LOW);
   Serial.println("NOT PUSHED!");
