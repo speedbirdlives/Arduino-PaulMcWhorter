832999623409148645e4c18c5bf10134e2e4edbb
Aditya Rao <speedbirdlives@gmail.com>
2026-09-14 11:20:47 +0530

    Update ServoLearn.ino
    Added necessary comments.

--- ServoLearn.ino
+++ ServoLearn.ino
@@ -1,17 +1,28 @@
+//Simple program to explain the usage of a Servo motor.
+
+
+//Include Servo extension package from the library
 #include <Servo.h>
 
-int servoPin = 10;
-int servoPos = 0;
+//Servo parameters
+int servoPin = 10; //Digital pin
+int servoPos = 0; //Servo position => Default set to 0 degrees
 
-Servo mySer;
+Servo mySer; //Defining and naming the servo motor
 
 void setup() {
   // put your setup code here, to run once:
+
+//Setting up serial monitor
 Serial.begin(9600);
+
+//Setting up Servo motor
 mySer.attach(servoPin);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+//Command to write position for Servo motor (in degrees) (with reference to default position => 0 degrees)
 mySer.write(servoPos);
 }
