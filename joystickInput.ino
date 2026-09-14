c91bcc383db7839406c1c1da172cb46e412d94f7
Aditya Rao <speedbirdlives@gmail.com>
2026-09-14 11:37:01 +0530

    Update joystickInput.ino
    Added necessary comments.

--- joystickInput.ino
+++ joystickInput.ino
@@ -1,11 +1,17 @@
+//Simple program to demonstrate input variables of a Joystick
 
-int XPin = A0;
-int YPin = A1;
-int Switch = 2;
+// (0,0) => (511,511)
+// +X=1023 , -X=0
+// +Y=1023 , -Y=0
+// Switch 0(pressed) or 1(not pressed)
 
-int VRX;
-int VRY;
-int Swi;
+int XPin = A0; //Xpin to analog pin
+int YPin = A1; //Ypin to analog pin
+int Switch = 2; //Click switch of the joystick to digital pin
+
+int VRX; //X-axis potentiometer analog reading
+int VRY; //Y-axis potentiometer analog reading
+int Swi; //JOystick button digital reading
 
 int wait = 2500;
 
@@ -14,6 +20,9 @@ int wait = 2500;
 
 void setup() {
   // put your setup code here, to run once:
+  
+  //Setting up pinModes and Serial monitor
+pinMode(Switch,INPUT);
 pinMode(XPin,INPUT);
 pinMode(YPin,INPUT);
 Serial.begin(9600);
@@ -21,17 +30,21 @@ Serial.begin(9600);
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+//analogRead and digitalRead through respective pins
 VRX = analogRead(XPin);
 VRY = analogRead(YPin);
 Swi = digitalRead(Switch);
 
-
+//X-axis
 Serial.print("X = ");
 Serial.println(VRX);
 
+//Y-axis
 Serial.print("Y = ");
 Serial.println(VRY);
 
+//Switch(button)
 Serial.println("Switch = ");
 Serial.println(Swi);
 
