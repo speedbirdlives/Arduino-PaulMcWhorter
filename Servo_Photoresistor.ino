d102fed2ec1a244f62b148c3df696c94be8c6aea
Aditya Rao <speedbirdlives@gmail.com>
2026-09-14 11:22:30 +0530

    Add Servo_Photoresistor.ino via upload.

---
+++ Servo_Photoresistor.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+
+int ServoPin = 10;
+int ReadPin = A0;
+
+int ServoPos;
+int reading;
+
+Servo mySer;
+
+void setup() {
+  // put your setup code here, to run once:
+
+Serial.begin(9600);
+mySer.attach(ServoPin); 
+pinMode(ReadPin,INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+reading = analogRead(ReadPin);
+Serial.println(reading);
+
+}
